09df92d6f194496789b3e6cc4f95bd09676fe8b6
katherynmk <katherynmknutson@lewisu.edu>
2022-01-29 17:25:51 -0600

    init

---
+++ dice.js
@@ -0,0 +1,11 @@
+function RollSixSidedDice() {
+    document.getElementById("dice").value = Math.floor(Math.random()*6) + 1;
+}
+function RollSixSidedDice1() {
+    document.getElementById("dice1").value = Math.floor(Math.random()*6) + 1;
+    
+}
+
+function Winner(){
+
+}
