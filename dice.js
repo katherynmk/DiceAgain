cd32bd113a89f536902716016c2bf9749fd1e1d6
katherynmk <katherynmknutson@lewisu.edu>
2022-01-31 14:00:24 -0600

    done

--- dice.js
+++ dice.js
@@ -1,16 +1,17 @@
 function RollSixSidedDice() {
+  //this just rolls dice for both boxes from the one button
   document.getElementById("dice").value = Math.floor(Math.random()*6) + 1;
   
   document.getElementById("dice1").value = Math.floor(Math.random()*6) + 1;
   
 
 }
-
+//this is the function that changes text + gives confetti for wins and ties
 
 function Winner(){
   let header = document.querySelector("p");
 var x = document.getElementById("dice").value;
-var y =  document.getElementById("dice1").value
+var y =  document.getElementById("dice1").value;
 
 if (x > y) {
   confetti();
